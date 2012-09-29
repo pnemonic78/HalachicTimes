a7c564248a37f4d5a3e0c11d1bd51885b70de03d
Moshe Waisberg <pnemonic@gmail.com>
2012-09-29 23:07:30 +0000

    cache repeated calculations. remove debug text views.

--- Halachic Times/src/net/sf/times/CompassView.java
+++ Halachic Times/src/net/sf/times/CompassView.java
@@ -36,7 +36,7 @@ import android.view.View;
  */
 public class CompassView extends View {
 
-	private static final int PADDING = 10;
+	private static final int PADDING = 0;
 	private static final int CIRCLE_THICKNESS = 2;
 
 	private float mNorth;
@@ -124,7 +124,7 @@ public class CompassView extends View {
 
 		mPaintWest = new TextPaint(mPaintEast);
 
-		mPaintNE = new TextPaint(mPaintEast);
+		mPaintNE = new Paint(mPaintEast);
 		mPaintNE.setColor(Color.DKGRAY);
 
 		mPaintHoliest = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -149,20 +149,25 @@ public class CompassView extends View {
 		final int w2 = w / 2;
 		final int h2 = h / 2;
 		final float r = Math.max(0, Math.min(w2, h2) - PADDING - CIRCLE_THICKNESS);
-		final float r2 = r / 2f;
-		final float r34 = (r * 3f) / 4f;
+		final float r5 = r / 2f;
+		final float r7 = (r * 7f) / 10f;
+		final float r8 = (r * 8f) / 10f;
+		final float r9 = (r * 9f) / 10f;
+		final float h2r5 = h2 - r5;
+		final float h2r7 = h2 - r7;
+		final float h2r8 = h2 - r8;
+		final float h2r9 = h2 - r9;
 
 		final float sizeDirections = r / 5f;
-		final float sizeDirections2 = sizeDirections / 2f;
 		mPaintNorth.setTextSize(sizeDirections);
 		mPaintEast.setTextSize(sizeDirections);
 		mPaintSouth.setTextSize(sizeDirections);
 		mPaintWest.setTextSize(sizeDirections);
 
-		mRectFill.left = w2 - r2;
-		mRectFill.top = h2 - r2;
-		mRectFill.right = w2 + r2;
-		mRectFill.bottom = h2 + r2;
+		mRectFill.left = w2 - r5;
+		mRectFill.top = h2 - r5;
+		mRectFill.right = w2 + r5;
+		mRectFill.bottom = h2 + r5;
 		mPaintFill.setStrokeWidth(r);
 		float sweepAngle = mNorth + mHoliest;
 		if (sweepAngle > 180f)
@@ -170,32 +175,32 @@ public class CompassView extends View {
 		canvas.drawArc(mRectFill, -90f, sweepAngle, false, mPaintFill);
 
 		canvas.rotate(mNorth, w2, h2);
-		canvas.drawLine(w2, h2 - r34, w2, h2 - r, mPaintNorth);
-		canvas.drawText(mLabelNorth, w2, h2 - r2, mPaintNorth);
+		canvas.drawLine(w2, h2r7, w2, h2r9, mPaintNorth);
+		canvas.drawText(mLabelNorth, w2, h2r5, mPaintNorth);
 
 		canvas.rotate(45, w2, h2);
-		canvas.drawLine(w2, h2 - r34, w2, h2 - r, mPaintNE);
+		canvas.drawLine(w2, h2r8, w2, h2r9, mPaintNE);
 
 		canvas.rotate(45, w2, h2);
-		canvas.drawLine(w2, h2 - r34, w2, h2 - r, mPaintEast);
-		canvas.drawText(mLabelEast, w2, h2 + sizeDirections2 - r2, mPaintEast);
+		canvas.drawLine(w2, h2r7, w2, h2r9, mPaintEast);
+		canvas.drawText(mLabelEast, w2, h2r5, mPaintEast);
 
 		canvas.rotate(45, w2, h2);
-		canvas.drawLine(w2, h2 - r34, w2, h2 - r, mPaintNE);
+		canvas.drawLine(w2, h2r8, w2, h2r9, mPaintNE);
 
 		canvas.rotate(45, w2, h2);
-		canvas.drawLine(w2, h2 - r34, w2, h2 - r, mPaintSouth);
-		canvas.drawText(mLabelSouth, w2, h2 - r2, mPaintSouth);
+		canvas.drawLine(w2, h2r7, w2, h2r9, mPaintSouth);
+		canvas.drawText(mLabelSouth, w2, h2r5, mPaintSouth);
 
 		canvas.rotate(45, w2, h2);
-		canvas.drawLine(w2, h2 - r34, w2, h2 - r, mPaintNE);
+		canvas.drawLine(w2, h2r8, w2, h2r9, mPaintNE);
 
 		canvas.rotate(45, w2, h2);
-		canvas.drawLine(w2, h2 - r34, w2, h2 - r, mPaintWest);
-		canvas.drawText(mLabelWest, w2, h2 + sizeDirections2 - r2, mPaintWest);
+		canvas.drawLine(w2, h2r7, w2, h2r9, mPaintWest);
+		canvas.drawText(mLabelWest, w2, h2r5, mPaintWest);
 
 		canvas.rotate(45, w2, h2);
-		canvas.drawLine(w2, h2 - r34, w2, h2 - r, mPaintNE);
+		canvas.drawLine(w2, h2r8, w2, h2r9, mPaintNE);
 
 		canvas.rotate(45 + mHoliest, w2, h2);
 		canvas.drawLine(w2, h2, w2, h2 - r, mPaintHoliest);
