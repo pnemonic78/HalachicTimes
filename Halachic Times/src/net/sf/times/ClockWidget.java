4b5a4c88686aefc5418b61f567c2332d0ef701e0
Moshe Waisberg <pnemonic@gmail.com>
2014-09-04 16:58:12 +0000

    bold clock hour.

--- Halachic Times/src/net/sf/times/ClockWidget.java
+++ Halachic Times/src/net/sf/times/ClockWidget.java
@@ -1,6 +1,18 @@
 package net.sf.times;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import net.sf.times.ZmanimAdapter.ZmanimItem;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.style.StyleSpan;
+import android.text.style.TypefaceSpan;
 import android.widget.RemoteViews;
 
 /**
@@ -10,6 +22,8 @@ import android.widget.RemoteViews;
  * @author Moshe
  */
 public class ClockWidget extends ZmanimWidget {
+
+	private DateFormat mTimeFormat;
 
 	/**
 	 * Constructs a new widget.
@@ -43,12 +57,31 @@ public class ClockWidget extends ZmanimWidget {
 
 	@Override
 	protected void bindView(RemoteViews views, ZmanimItem item) {
-		views.setTextViewText(R.id.time, item.timeLabel);
+		if (mTimeFormat == null) {
+			Context context = mContext;
+			boolean time24 = android.text.format.DateFormat.is24HourFormat(context);
+			String pattern = context.getString(time24 ? R.string.clock_24_hours_format : R.string.clock_12_hours_format);
+			mTimeFormat = new SimpleDateFormat(pattern, Locale.getDefault());
+		}
+
+		CharSequence label = mTimeFormat.format(item.time);
+		SpannableStringBuilder spans = new SpannableStringBuilder(label);
+		int indexMinutes = TextUtils.indexOf(label, ':');
+		spans.setSpan(new TypefaceSpan("sans-serif"), 0, indexMinutes, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+		spans.setSpan(new StyleSpan(Typeface.BOLD), 0, indexMinutes, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+		views.setTextViewText(R.id.time, spans);
 		views.setTextViewText(android.R.id.title, mContext.getText(item.titleId));
 	}
 
 	@Override
 	protected boolean isRemoteList() {
 		return false;
+	}
+
+	@Override
+	@SuppressLint("NewApi")
+	protected void notifyAppWidgetViewDataChanged(Context context) {
+		mTimeFormat = null;
+		super.notifyAppWidgetViewDataChanged(context);
 	}
 }
