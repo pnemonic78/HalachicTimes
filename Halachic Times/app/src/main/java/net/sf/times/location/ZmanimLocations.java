0084e31aad6f44ab31034c27125ad0c5d291a197
Moshe Waisberg <pnemonic@gmail.com>
2016-02-01 19:22:03 +0000

    give GPS more chance to find location.

--- Halachic Times/app/src/main/java/net/sf/times/location/ZmanimLocations.java
+++ Halachic Times/app/src/main/java/net/sf/times/location/ZmanimLocations.java
@@ -66,17 +66,17 @@ public class ZmanimLocations implements ZmanimLocationListener {
     /** ISO 639 language code for "Yiddish". */
     public static final String ISO639_YIDDISH = "yi";
 
-    /** The minimum time interval between location updates, in milliseconds. */
-    private static final long UPDATE_TIME = 30 * DateUtils.SECOND_IN_MILLIS;
     /** The maximum time interval between location updates, in milliseconds. */
-    private static final long UPDATE_TIME_MAX = 2 * DateUtils.HOUR_IN_MILLIS;
+    private static final long UPDATE_TIME_MAX = 6 * DateUtils.HOUR_IN_MILLIS;
     /** The time interval between requesting location updates, in milliseconds. */
     private static final long UPDATE_TIME_START = 30 * DateUtils.SECOND_IN_MILLIS;
     /**
      * The duration to receive updates, in milliseconds.<br>
      * Should be enough time to get a sufficiently accurate location.
      */
-    private static final long UPDATE_DURATION = 30 * DateUtils.SECOND_IN_MILLIS;
+    private static final long UPDATE_DURATION = DateUtils.MINUTE_IN_MILLIS;
+    /** The minimum time interval between location updates, in milliseconds. */
+    private static final long UPDATE_TIME = 15 * DateUtils.SECOND_IN_MILLIS;
     /** The minimum distance between location updates, in metres. */
     private static final int UPDATE_DISTANCE = 100;
 
