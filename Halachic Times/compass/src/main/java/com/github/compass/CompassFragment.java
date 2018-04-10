4e080063fa79e5afdefe8f4439d82b4c9441b10a
Moshe <pnemonic@gmail.com>
2018-04-10 15:16:36 +0300

    migrate to GitHub

--- Halachic Times/compass/src/main/java/com/github/compass/CompassFragment.java
+++ Halachic Times/compass/src/main/java/com/github/compass/CompassFragment.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.sf.compass;
+package com.github.compass;
 
 import android.content.Context;
 import android.content.res.TypedArray;
