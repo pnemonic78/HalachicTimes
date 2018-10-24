a611c367a6267a56580d1c90c250f7a981db7444
Moshe <pnemonic@gmail.com>
2018-10-24 17:36:19 +0300

    GeoNames library - toponym

--- Halachic Times/locations/src/main/java/com/github/times/location/geonames/GeoNamesRecord.java
+++ Halachic Times/locations/src/main/java/com/github/times/location/geonames/GeoNamesRecord.java
@@ -15,6 +15,9 @@
  */
 package com.github.times.location.geonames;
 
+import org.geonames.InsufficientStyleException;
+import org.geonames.Toponym;
+
 import static com.github.times.location.ZmanimAddress.ISO639_ISRAEL;
 import static com.github.times.location.ZmanimAddress.ISO639_PALESTINE;
 
@@ -23,99 +26,12 @@ import static com.github.times.location.ZmanimAddress.ISO639_PALESTINE;
  *
  * @author Moshe Waisberg
  */
-public class GeoNamesRecord {
+public class GeoNamesRecord extends Toponym {
 
-    /** Feature class for a populated city. */
-    public static final String FEATURE_P = "P";
-    /** Feature code for a populated place. */
-    public static final String FEATURE_PPL = "PPL";
-    /** Feature code for a seat of a first-order administrative division. */
-    public static final String FEATURE_PPLA = "PPLA";
-    /** Feature code for a seat of a second-order administrative division. */
-    public static final String FEATURE_PPLA2 = "PPLA2";
-    /** Feature code for a seat of a third-order administrative division. */
-    public static final String FEATURE_PPLA3 = "PPLA3";
-    /** Feature code for a seat of a fourth-order administrative division. */
-    public static final String FEATURE_PPLA4 = "PPLA4";
-    /** Feature code for a capital of a political entity. */
-    public static final String FEATURE_PPLC = "PPLC";
-    /** Feature code for a farm village. */
-    public static final String FEATURE_PPLF = "PPLF";
-    /** Feature code for a seat of government of a political entity. */
-    public static final String FEATURE_PPLG = "PPLG";
-    /** Feature code for a populated locality. */
-    public static final String FEATURE_PPLL = "PPLL";
-    /** Feature code for an abandoned populated place. */
-    public static final String FEATURE_PPLQ = "PPLQ";
-    /** Feature code for a religious populated place. */
-    public static final String FEATURE_PPLR = "PPLR";
-    /** Feature code for populated places. */
-    public static final String FEATURE_PPLS = "PPLS";
-    /** Feature code for a destroyed populated place. */
-    public static final String FEATURE_PPLW = "PPLW";
-    /** Feature code for a section of populated place. */
-    public static final String FEATURE_PPLX = "PPLX";
-    /** Feature code for an Israeli settlement. */
-    public static final String FEATURE_STLMT = "STLMT";
-
-    private long geonameId;
-    private String name;
     private String asciiName;
-    private String alternateNames;
-    private double latitude;
-    private double longitude;
-    private String featureClass;
-    private String featureCode;
-    private String countryCode;
     private String cc2;
-    private String adminCode1;
-    private String adminCode2;
-    private String adminCode3;
-    private String adminCode4;
-    private String adminCode5;
-    private long population;
-    private int elevation = Integer.MIN_VALUE;
     private int dem = Integer.MIN_VALUE;
-    private String timezone;
     private String modification;
-
-    /**
-     * Get the geoname id.
-     *
-     * @return the id.
-     */
-    public long getGeoNameId() {
-        return geonameId;
-    }
-
-    /**
-     * Set the geoname id.
-     *
-     * @param geonameId
-     *         the id.
-     */
-    public void setGeoNameId(long geonameId) {
-        this.geonameId = geonameId;
-    }
-
-    /**
-     * Get the name.
-     *
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Set the name.
-     *
-     * @param name
-     *         the name.
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
 
     /**
      * Get the ASCII name.
@@ -129,128 +45,23 @@ public class GeoNamesRecord {
     /**
      * Set the ASCII name.
      *
-     * @param asciiName
-     *         the name.
+     * @param asciiName the name.
      */
     public void setAsciiName(String asciiName) {
         this.asciiName = asciiName;
     }
 
     /**
-     * Get the alternate names.
-     *
-     * @return the alternateNames
-     */
-    public String getAlternateNames() {
-        return alternateNames;
-    }
-
-    /**
-     * Set the alternate names.
-     *
-     * @param alternateNames
-     *         the alternate names.
-     */
-    public void setAlternateNames(String alternateNames) {
-        this.alternateNames = alternateNames;
-    }
-
-    /**
-     * Get the latitude.
-     *
-     * @return the latitude
-     */
-    public double getLatitude() {
-        return latitude;
-    }
-
-    /**
-     * Set the latitude.
-     *
-     * @param latitude
-     *         the latitude.
-     */
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    /**
-     * Get the longitude.
-     *
-     * @return the longitude
-     */
-    public double getLongitude() {
-        return longitude;
-    }
-
-    /**
-     * Set the longitude.
-     *
-     * @param longitude
-     *         the longitude.
-     */
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    /**
-     * Get the featureClass.
-     *
-     * @return the featureClass
-     */
-    public String getFeatureClass() {
-        return featureClass;
-    }
-
-    /**
-     * Set the featureClass.
-     *
-     * @param featureClass
-     *         the featureClass.
-     */
-    public void setFeatureClass(String featureClass) {
-        this.featureClass = featureClass;
-    }
-
-    /**
-     * Get the featureCode.
-     *
-     * @return the featureCode
-     */
-    public String getFeatureCode() {
-        return featureCode;
-    }
-
-    /**
-     * Set the featureCode.
-     *
-     * @param featureCode
-     *         the featureCode.
-     */
-    public void setFeatureCode(String featureCode) {
-        this.featureCode = featureCode;
-    }
-
-    /**
-     * Get the countryCode.
-     *
-     * @return the countryCode
-     */
-    public String getCountryCode() {
-        return countryCode;
-    }
-
-    /**
      * Set the countryCode.
      *
-     * @param countryCode
-     *         the countryCode.
+     * @param countryCode the countryCode.
      */
+    @Override
     public void setCountryCode(String countryCode) {
         if (ISO639_PALESTINE.equals(countryCode)) {
             countryCode = ISO639_ISRAEL;
         }
-        this.countryCode = countryCode;
+        super.setCountryCode(countryCode);
     }
 
     /**
@@ -265,144 +76,10 @@ public class GeoNamesRecord {
     /**
      * Set the alternate country codes.
      *
-     * @param cc2
-     *         the the comma-separated list of codes.
+     * @param cc2 the the comma-separated list of codes.
      */
     public void setAlternateCountryCodes(String cc2) {
         this.cc2 = cc2;
-    }
-
-    /**
-     * Get the admin 1 code.
-     *
-     * @return the code.
-     */
-    public String getAdminCode1() {
-        return adminCode1;
-    }
-
-    /**
-     * Set the admin 1 code.
-     *
-     * @param code
-     *         the code.
-     */
-    public void setAdminCode1(String code) {
-        this.adminCode1 = code;
-    }
-
-    /**
-     * Get the admin 2 code.
-     *
-     * @return the code.
-     */
-    public String getAdminCode2() {
-        return adminCode2;
-    }
-
-    /**
-     * Set the admin 2 code.
-     *
-     * @param code
-     *         the code.
-     */
-    public void setAdminCode2(String code) {
-        this.adminCode2 = code;
-    }
-
-    /**
-     * Get the admin 3 code.
-     *
-     * @return the code.
-     */
-    public String getAdminCode3() {
-        return adminCode3;
-    }
-
-    /**
-     * Set the admin 3 code.
-     *
-     * @param code
-     *         the code.
-     */
-    public void setAdminCode3(String code) {
-        this.adminCode3 = code;
-    }
-
-    /**
-     * Get the admin 4 code.
-     *
-     * @return the code.
-     */
-    public String getAdminCode4() {
-        return adminCode4;
-    }
-
-    /**
-     * Set the admin 5 code.
-     *
-     * @param code
-     *         the code.
-     */
-    public void setAdminCode4(String code) {
-        this.adminCode4 = code;
-    }
-
-    /**
-     * Get the admin 5 code.
-     *
-     * @return the code.
-     */
-    public String getAdminCode5() {
-        return adminCode5;
-    }
-
-    /**
-     * Set the admin 4 code.
-     *
-     * @param code
-     *         the code.
-     */
-    public void setAdminCode5(String code) {
-        this.adminCode5 = code;
-    }
-
-    /**
-     * Get the population.
-     *
-     * @return the population
-     */
-    public long getPopulation() {
-        return population;
-    }
-
-    /**
-     * Set the population.
-     *
-     * @param population
-     *         the population.
-     */
-    public void setPopulation(long population) {
-        this.population = population;
-    }
-
-    /**
-     * Get the elevation.
-     *
-     * @return the elevation
-     */
-    public int getElevation() {
-        return elevation;
-    }
-
-    /**
-     * Set the elevation.
-     *
-     * @param elevation
-     *         the elevation.
-     */
-    public void setElevation(int elevation) {
-        this.elevation = elevation;
     }
 
     /**
@@ -417,30 +94,10 @@ public class GeoNamesRecord {
     /**
      * Set the SRTM3 elevation.
      *
-     * @param dem
-     *         the elevation.
+     * @param dem the elevation.
      */
     public void setDigitalElevation(int dem) {
         this.dem = dem;
-    }
-
-    /**
-     * Get the timezone.
-     *
-     * @return the timezone
-     */
-    public String getTimeZone() {
-        return timezone;
-    }
-
-    /**
-     * Set the timezone.
-     *
-     * @param timezone
-     *         the timezone.
-     */
-    public void setTimeZone(String timezone) {
-        this.timezone = timezone;
     }
 
     /**
@@ -455,8 +112,7 @@ public class GeoNamesRecord {
     /**
      * Set the modification.
      *
-     * @param modification
-     *         the modification.
+     * @param modification the modification.
      */
     public void setModification(String modification) {
         this.modification = modification;
@@ -464,12 +120,17 @@ public class GeoNamesRecord {
 
     @Override
     public int hashCode() {
-        return (int) getGeoNameId();
+        return getGeoNameId();
     }
 
-    public int getGrossElevation() {
-        int elevation = getElevation();
-        if (elevation == Integer.MIN_VALUE) {
+    public Integer getGrossElevation() {
+        Integer elevation = null;
+        try {
+            elevation = getElevation();
+        } catch (InsufficientStyleException e) {
+            e.printStackTrace();
+        }
+        if (elevation == null) {
             elevation = getDigitalElevation();
         }
         return elevation;
