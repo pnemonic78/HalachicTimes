d0b3a3ddbbdd01c56137c0a1665e98c13110d2ee
M. Waisberg <pnemonic@gmail.com>
2019-05-15 13:32:31 +0300

    test sof zman kidddush levana

---
+++ Halachic Times/app/src/test/java/com/github/times/ZmanimTests.java
@@ -0,0 +1,43 @@
+package com.github.times;
+
+import net.sourceforge.zmanim.ComplexZmanimCalendar;
+import net.sourceforge.zmanim.util.GeoLocation;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+public class ZmanimTests {
+
+    @Test
+    public void sofZmanKidushLevana() {
+        // Jerusalem, Israel, 2019-May
+        TimeZone tz = TimeZone.getTimeZone("Asia/Jerusalem");
+        Calendar cal = Calendar.getInstance(tz);
+        cal.set(2019, Calendar.MAY, 1);
+        GeoLocation location = new GeoLocation("test", 31.76904, 35.21633, tz);
+        ComplexZmanimCalendar zcal = new ComplexZmanimCalendar(location);
+        zcal.setCalendar(cal);
+        Long date = zcal.getSofZmanKidushLevanaBetweenMoldos();
+        assertNull(date);
+
+        cal.set(2019, Calendar.MAY, 19);
+        zcal.setCalendar(cal);
+        date = zcal.getSofZmanKidushLevanaBetweenMoldos();
+        assertNotNull(date);
+        assertEquals(1558246265170L, date.longValue());
+        cal.setTimeInMillis(date);
+        assertEquals(cal.get(Calendar.YEAR), 2019);
+        assertEquals(cal.get(Calendar.MONTH), Calendar.MAY);
+        assertEquals(cal.get(Calendar.DAY_OF_MONTH), 19);
+        assertEquals(cal.get(Calendar.HOUR_OF_DAY), 9);
+        assertEquals(cal.get(Calendar.MINUTE), 11);
+        assertEquals(cal.get(Calendar.SECOND), 5);
+    }
+
+}
