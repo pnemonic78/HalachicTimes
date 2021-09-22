ead25fc645718da3f6e20b6ded15b7502a89f62f
Moshe W <pnemonic@gmail.com>
2021-09-22 22:09:15 +0300

    gradle tools 7.0.2

--- Halachic Times/build.gradle.kts
+++ Halachic Times/build.gradle.kts
@@ -6,11 +6,11 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0")
+        classpath("com.android.tools.build:gradle:7.0.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${BuildVersions.kotlin_version}")
 
         // Crashlytics
-        classpath("com.google.gms:google-services:4.3.8")
+        classpath("com.google.gms:google-services:4.3.10")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.7.1")
     }
 }
